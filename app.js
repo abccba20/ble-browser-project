20ca8a98e7797b97b574f27578b689bd07c1d1c6
unknown <abc.cba.20@outlook.es>
2020-11-20 12:55:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+app = express()
+
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+app.use((req, res, next)=>{
+  console.log(req.path)
+  next()
+})
+
+app.set('views', (__dirname +'/public/views')); //directory of views
+//app.engine('html', require('ejs').renderFile); //if not 'cannot find module html'
+app.set('view engine', 'html')
+//app.set('view engine', 'html') //default extension for views
+
+app.use(express.static(__dirname +'/public'));
+
+/*app.use(function (req, res, next) {
+	//comprobar login console.log('Time:', Date.now());
+	if (true) {
+		sign = 'parts/goIn'
+	} else{
+		sign = 'parts/goOut'
+	}
+  next();
+});*/
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/public/views/index.html'))
+})
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+})
+
+
+app.listen(port, function() {
+	console.log('Server on port ',port);
+	//log.info('Servidor escuchando a través del puerto ',port)
+})
